0410f22a9d14f10d6d8fba4cebe2699dfe0ec0bd
MoonJuhan <codemike@naver.com>
2024-07-11 20:10:08 +0900

    refactor: Add prettier

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -1,20 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist",
+    outDir: 'dist',
     rollupOptions: {
       output: {
-        format: "esm",
-        entryFileNames: "[name].js",
-        chunkFileNames: "[name].js",
-        assetFileNames: "[name].[ext]",
+        format: 'esm',
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
       },
     },
   },
   server: {
     port: 3000,
   },
-});
+})
--- vue-app/vite.config.js
+++ vue-app/vite.config.js
@@ -1,20 +1,20 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: "dist",
+    outDir: 'dist',
     rollupOptions: {
       output: {
-        format: "esm",
-        entryFileNames: "[name].js",
-        chunkFileNames: "[name].js",
-        assetFileNames: "[name].[ext]",
+        format: 'esm',
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
       },
     },
   },
   server: {
     port: 8080,
   },
-});
+})
