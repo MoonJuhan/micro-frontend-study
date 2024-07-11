ba31d13fdcd183239fdc57bdcdf84db3dbc7fa88
MoonJuhan <codemike@naver.com>
2024-07-11 23:16:08 +0900

    refactor: Code cleanup

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -3,17 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        format: 'esm',
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]',
-      },
-    },
-  },
   server: {
     port: 3000,
   },
--- vue-app/vite.config.js
+++ vue-app/vite.config.js
@@ -3,17 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        format: 'esm',
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]',
-      },
-    },
-  },
   server: {
     port: 8080,
   },
