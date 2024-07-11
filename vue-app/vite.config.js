a9c0611eb6d5de49a3129cb830280deeb3ad2a01
MoonJuhan <codemike@naver.com>
2024-07-11 20:00:38 +0900

    feat: Add vite config in vue, react

--- vue-app/vite.config.js
+++ vue-app/vite.config.js
@@ -1,16 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+  plugins: [vue()],
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        format: "esm",
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
+      },
+    },
+  },
+  server: {
+    port: 8080,
+  },
+});
